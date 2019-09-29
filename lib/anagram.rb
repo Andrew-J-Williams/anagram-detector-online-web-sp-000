ddaf8af7929a9cbcdbe66d40570a531f72e219c1
Andrew Williams <1williamsaj@gmail.com>
2019-09-29 21:38:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     name_array.select {|words| words.split("").sort == @name.split("").sort}
     # The iterator above takes each element in the name array and splits each letter of the selected word. Then it sorts the letters
     # in the new array. It then compares that word to our instance variable word, which is split and sorted the same way.
-    # 
+    # It will finish by returning an array of all values that are anagrams of the 'name' variable.
   end
 
 end
