188cfb69d96be88a494178117f05bc2599c77389
Andrew Williams <1williamsaj@gmail.com>
2019-09-29 21:37:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,11 @@ class Anagram
   end
 
   def match(name_array)
-    # 
+    # We use the select method to return all elements that return 'true' to the following conditions
     name_array.select {|words| words.split("").sort == @name.split("").sort}
-
-
+    # The iterator above takes each element in the name array and splits each letter of the selected word. Then it sorts the letters
+    # in the new array. It then compares that word to our instance variable word, which is split and sorted the same way.
+    # 
   end
 
 end
